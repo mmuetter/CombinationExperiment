9ca8a9a39b9ea5abb31bd1de97b89708e5eecc6a
mmuetter <malte.muetter@env.ethz.ch>
2025-02-04 16:04:54 +0100

    debugged plate prep

--- main_prep.py
+++ main_prep.py
@@ -1,7 +1,7 @@
 from pypetting_extra import Experiment
 from configurations import AssayConfiguration
-from setupDrugPlates import DrugPlateSetup
-from prepWorkflow import PrepWorkflow
+from setup_drug_plates import DrugPlateSetup
+from workflow_prep import PrepWorkflow
 from general_classes import PathManager
 import pandas as pd
 import numpy as np
@@ -9,8 +9,8 @@ from functions import layout_antibiotic_plate
 
 ## use pyenv 3.12.0
 
-folder = "twofold1:1"
-exp_name = "prep_plates_03022025"
+folder = "twofold1to1"
+exp_name = "prep_plates_04022025"
 exp_path = "/Users/malte/polybox/Shared/Robot-Malte/CombinationProject/" + folder
 
 pm = PathManager(basepath="current")
@@ -25,16 +25,29 @@ experiment = Experiment(
 config = AssayConfiguration()
 setup = DrugPlateSetup(config, experiment)
 workflow = PrepWorkflow(setup)
-workflow._prefill_drug_reservoir()
-prefix = "combine_1:1_"
+workflow.prefill_drug_reservoir()
+workflow.dilution_row_drug_reservoir()
+prefix = "combine_"
 
 base_df = layout_antibiotic_plate(drugs, 0.5, 0.5)
 mic_dict = dict(zip(drugs.drug, drugs.micRef))
-
+[conc for conc in config.concentration_gradient]
+cols = config.concentration_gradient
+cols.sort(reverse=True)
+names = [f"antibiotics{"".join(str(s).split(".")) }" for s in cols]
+concentration_col_dict = dict(
+    zip(names, np.array(range(len(config.concentration_gradient))) + 1)
+)
 for plate, name, rel_conc in zip(
     setup.antibiotic_plates, config.plate_names, config.concentration_gradient
 ):
-    workflow.combine_drugs(plate, 0.5, 0.5, prefix + name)
+    src_col = concentration_col_dict[name]
+    workflow.combine_drugs(plate, 0.5, 0.5, prefix + name, src_col)
     df = base_df.copy()
     df["rel_conc"] = rel_conc
     experiment.save_csv(df, "setup_" + name + ".csv")
+
+drugs["cmax_mic"] = 32
+drugs["cmax"] = drugs.cmax_mic * drugs.micRef
+drugs["V_stock_ul"] = drugs.cmax / (drugs.stock)
+experiment.save_csv(drugs, "fill_table.csv")
--- workflow_prep.py
+++ workflow_prep.py
@@ -1,4 +1,4 @@
-from setup_pdcurve import (
+from setup_drug_plates import (
     liha,
     roma,
     tilter,
@@ -9,13 +9,13 @@ from setup_pdcurve import (
     lid2_pos,
     lid3_pos,
     rotated_site,
-    AssaySetup,
 )
+from dataclasses import dataclass
 import numpy as np
 
 
 class PrepWorkflow:
-    def __init__(self, setup: AssaySetup):
+    def __init__(self, setup: dataclass):
         self.experiment = experiment = setup.experiment
         self.setup = setup
         self.config = setup.config
@@ -23,9 +23,9 @@ class PrepWorkflow:
         self.tip_arr = [False] + 6 * [True] + [False]
         self.column_mask96 = self.tip_arr
         self.column_mask384 = np.array(2 * [False] + 6 * [True, False] + 2 * [False])
-        self.column_mask_rotated = np.array(6 * [False, True] + [False, False])
+        self.column_mask96_rotated = np.array([False] + 6 * [True] + 5 * [False])
 
-    def _prefill_drug_reservoir(self):
+    def prefill_drug_reservoir(self):
         wl = self.setup_worklist("prefill_drug_reservoir.gwl")
         wl.add(liha.sterile_wash())
 
@@ -42,11 +42,14 @@ class PrepWorkflow:
                 end_col=len(self.config.concentration_gradient),
             )
         )
+
+    def dilution_row_drug_reservoir(self):
+        wl = self.setup_worklist("dilution_row_drug_reservoir.gwl")
         wl.add(
             liha.dilution_row(
                 self.setup.drug_reservoir,
                 self.column_mask96,
-                self.config.overnight_final_volume * 2,
+                self.config.antibiotic_reservoir_plate_final_vol * 2,
                 start_col=1,
                 stop_at_col=len(self.config.concentration_gradient) - 1,
                 dilution_factor=2,
@@ -55,7 +58,9 @@ class PrepWorkflow:
         )
         wl.add(liha.sterile_wash())
 
-    def combine_drugs(self, plate, pA, pB, label):
+    def combine_drugs(self, plate, pA, pB, label, src_col):
+        if pA + pB != 1:
+            raise ValueError("pA + pB must be 1")
         wl = self.setup_worklist(label + ".gwl")
         wl.add(liha.sterile_wash())
         wl.add(
@@ -74,11 +79,12 @@ class PrepWorkflow:
                 plate,
                 vol_A,
                 self.column_mask96,
-                self.column_mask_rotated,
+                self.column_mask96,
                 liquid_class="Minimal FD ZMAX",
                 tip_array=self.tip_arr,
                 start_col=2,
                 end_col=7,
+                src_col=src_col,
             )
         )
 
@@ -97,11 +103,12 @@ class PrepWorkflow:
                 plate,
                 vol_B,
                 self.column_mask96,
-                self.column_mask_rotated,
+                self.column_mask96_rotated,
                 liquid_class="Minimal FD ZMAX",
                 tip_array=self.tip_arr,
                 start_col=2,
                 end_col=7,
+                src_col=src_col,
             )
         )
         wl.add(roma.store(plate))
